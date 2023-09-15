c43bf8347d5b8ffcae67347254ad63fb0b7e7f2f
Jonah Werre <jonahwerre@gmail.com>
2023-09-15 08:56:39 -0600

    removed # from id property

--- src/index.ts
+++ src/index.ts
@@ -1,101 +1,109 @@
 interface Headers {
-  headerText: string;
-  id: string;
-  children: Headers[];
+	headerText: string;
+	id: string;
+	children: Headers[];
 }
 
-type ContentType = "md" | "html";
+type ContentType = 'md' | 'html';
 
 const getHeadersFromHTML = (html: string) => {
-  const headerRegex = /<h[1-6][^>]*>[\s\S]*?<\/h[1-6]>/g;
-  const headers = html.match(headerRegex);
-  return headers;
+	const headerRegex = /<h[1-6][^>]*>[\s\S]*?<\/h[1-6]>/g;
+	const headers = html.match(headerRegex);
+	return headers;
 };
 
 const getHeadersFromMarkdown = (markdown: string) => {
-  // load markdown
-  const headerRegex = /#{1,6}.*\n/g;
-  const headers = markdown.match(headerRegex);
+	// load markdown
+	const headerRegex = /#{1,6}.*\n/g;
+	const headers = markdown.match(headerRegex);
 
-  return headers;
+	return headers;
 };
 
 // get headers from html, or markdown
 export const inputHeaders = (source: string, type: ContentType) => {
-  if (type === "md") {
-    return getHeadersFromMarkdown(source);
-  }
-  if (type === "html") {
-    return getHeadersFromHTML(source);
-  }
+	if (type === 'md') {
+		return getHeadersFromMarkdown(source);
+	}
+	if (type === 'html') {
+		return getHeadersFromHTML(source);
+	}
 };
 
 export const parseHeaders = (source: string, type: ContentType): Headers[] => {
-  // get all headers
-  const htmlHeaders = inputHeaders(source, type);
-  if (!htmlHeaders || !htmlHeaders.length) {
-    return [];
-  }
+	// get all headers
+	const htmlHeaders = inputHeaders(source, type);
+	if (!htmlHeaders || !htmlHeaders.length) {
+		return [];
+	}
 
-  // nest headers
-  const headers: Headers[] | undefined = [];
-  let currentHeaderArray: Headers[] | undefined = headers;
-  let currentLevel = 0;
-  if (type === "md") {
-    currentLevel = htmlHeaders[0].split("#").length - 1;
-  } else {
-    currentLevel = parseInt(htmlHeaders[0].charAt(2));
-  }
-  const levelController = currentLevel - 1;
+	// nest headers
+	const headers: Headers[] | undefined = [];
+	let currentHeaderArray: Headers[] | undefined = headers;
+	let currentLevel = 0;
+	if (type === 'md') {
+		currentLevel = htmlHeaders[0].split('#').length - 1;
+	} else {
+		currentLevel = parseInt(htmlHeaders[0].charAt(2));
+	}
+	const levelController = currentLevel - 1;
 
-  for (const header of htmlHeaders) {
-    let headerLevel: number;
-    let headerText: string;
+	for (const header of htmlHeaders) {
+		let headerLevel: number;
+		let headerText: string;
 
-    if (type === "md") {
-      headerLevel = header.split("#").length - 1;
-    } else {
-      headerLevel = parseInt(header.charAt(2));
-    }
+		if (type === 'md') {
+			headerLevel = header.split('#').length - 1;
+		} else {
+			headerLevel = parseInt(header.charAt(2));
+		}
 
-    if (type === "md") {
-      headerText = header
-        .replace(/#{1,6}/g, "")
-        .replace(/\n/g, "")
-        .replace(/ {2}/g, " ")
-        .trim();
-    } else {
-      headerText = header
-        .replace(/<[^>]*>/g, "")
-        .trim()
-        .replace(/\n\s*/g, " ")
-        .replace(/ {2}/g, " ");
-    }
+		if (type === 'md') {
+			headerText = header
+				.replace(/#{1,6}/g, '')
+				.replace(/\n/g, '')
+				.replace(/ {2}/g, ' ')
+				.trim();
+		} else {
+			headerText = header
+				.replace(/<[^>]*>/g, '')
+				.trim()
+				.replace(/\n\s*/g, ' ')
+				.replace(/ {2}/g, ' ');
+		}
 
-    const id = `#${headerText
-      .replace(/[^\w\s-]|_/g, "")
-      .replace(/\s/g, "-")
-      .replace(/:/g, "")
+		const id = `${headerText
+			.replace(/[^\w\s-]|_/g, '')
+			.replace(/\s/g, '-')
+			.replace(/:/g, '')
 
-      .toLowerCase()}`;
-    if (headerLevel === currentLevel && currentHeaderArray) {
-      currentHeaderArray.push({ headerText: headerText, id, children: [] });
-    } else if (headerLevel > currentLevel && currentHeaderArray) {
-      currentHeaderArray[currentHeaderArray.length - 1].children = [
-        { headerText, id, children: [] },
-      ];
-      currentHeaderArray =
-        currentHeaderArray[currentHeaderArray.length - 1].children;
-      currentLevel = headerLevel;
-    } else if (headerLevel < currentLevel) {
-      currentHeaderArray = headers;
-      for (let i = 1; i < headerLevel - levelController; i++) {
-        currentHeaderArray =
-          currentHeaderArray[currentHeaderArray.length - 1].children;
-      }
-      currentLevel = headerLevel;
-      currentHeaderArray.push({ headerText: headerText, id, children: [] });
-    }
-  }
-  return headers;
+			.toLowerCase()}`;
+		if (headerLevel === currentLevel && currentHeaderArray) {
+			currentHeaderArray.push({
+				headerText: headerText,
+				id,
+				children: [],
+			});
+		} else if (headerLevel > currentLevel && currentHeaderArray) {
+			currentHeaderArray[currentHeaderArray.length - 1].children = [
+				{ headerText, id, children: [] },
+			];
+			currentHeaderArray =
+				currentHeaderArray[currentHeaderArray.length - 1].children;
+			currentLevel = headerLevel;
+		} else if (headerLevel < currentLevel) {
+			currentHeaderArray = headers;
+			for (let i = 1; i < headerLevel - levelController; i++) {
+				currentHeaderArray =
+					currentHeaderArray[currentHeaderArray.length - 1].children;
+			}
+			currentLevel = headerLevel;
+			currentHeaderArray.push({
+				headerText: headerText,
+				id,
+				children: [],
+			});
+		}
+	}
+	return headers;
 };
--- tests/markdown.test.ts
+++ tests/markdown.test.ts
@@ -1,85 +1,113 @@
-import { it, describe, expect } from "vitest";
-import { readFile } from "node:fs/promises";
-import { parseHeaders } from "../src/index";
+import { it, describe, expect } from 'vitest';
+import { readFile } from 'node:fs/promises';
+import { parseHeaders } from '../src/index';
 
-describe("Parse html string to headers", () => {
-  it("should parse empty markdown string", () => {
-    const res = parseHeaders("", "md");
-    expect(res).toBeInstanceOf(Array);
-    expect(res.length).toBe(0);
-  });
+describe('Parse html string to headers', () => {
+	it('should parse empty markdown string', () => {
+		const res = parseHeaders('', 'md');
+		expect(res).toBeInstanceOf(Array);
+		expect(res.length).toBe(0);
+	});
 
-  it("should parse markdown without headers", () => {
-    const markdown = "Some Markdown!!";
-    const res = parseHeaders(markdown, "md");
-    expect(res).toBeInstanceOf(Array);
-    expect(res.length).toBe(0);
-  });
+	it('should parse markdown without headers', () => {
+		const markdown = 'Some Markdown!!';
+		const res = parseHeaders(markdown, 'md');
+		expect(res).toBeInstanceOf(Array);
+		expect(res.length).toBe(0);
+	});
 
-  it("should parse markdown table of contents", async () => {
-    const markdown = await readFile("tests/fixtures/test.md", "utf-8");
+	it('should parse markdown table of contents', async () => {
+		const markdown = await readFile('tests/fixtures/test.md', 'utf-8');
 
-    const res = parseHeaders(markdown, "md");
-    expect(res).toStrictEqual([
-      {
-        headerText: "Header 1",
-        id: "#header-1",
-        children: [
-          {
-            headerText: "Header 2",
-            id: "#header-2",
-            children: [
-              { headerText: "Header 3", id: "#header-3", children: [] },
-              { headerText: "Header 3", id: "#header-3", children: [] },
-              { headerText: "Header 3", id: "#header-3", children: [] },
-            ],
-          },
-          { headerText: "Header 2", id: "#header-2", children: [] },
-          {
-            headerText: "Header 2",
-            id: "#header-2",
-            children: [
-              { headerText: "Header 3", id: "#header-3", children: [] },
-              { headerText: "Header 3", id: "#header-3", children: [] },
-              { headerText: "Header 3", id: "#header-3", children: [] },
-            ],
-          },
-          { headerText: "Header 2", id: "#header-2", children: [] },
-        ],
-      },
-    ]);
-  });
+		const res = parseHeaders(markdown, 'md');
+		expect(res).toStrictEqual([
+			{
+				headerText: 'Header 1',
+				id: 'header-1',
+				children: [
+					{
+						headerText: 'Header 2',
+						id: 'header-2',
+						children: [
+							{
+								headerText: 'Header 3',
+								id: 'header-3',
+								children: [],
+							},
+							{
+								headerText: 'Header 3',
+								id: 'header-3',
+								children: [],
+							},
+							{
+								headerText: 'Header 3',
+								id: 'header-3',
+								children: [],
+							},
+						],
+					},
+					{ headerText: 'Header 2', id: 'header-2', children: [] },
+					{
+						headerText: 'Header 2',
+						id: 'header-2',
+						children: [
+							{
+								headerText: 'Header 3',
+								id: 'header-3',
+								children: [],
+							},
+							{
+								headerText: 'Header 3',
+								id: 'header-3',
+								children: [],
+							},
+							{
+								headerText: 'Header 3',
+								id: 'header-3',
+								children: [],
+							},
+						],
+					},
+					{ headerText: 'Header 2', id: 'header-2', children: [] },
+				],
+			},
+		]);
+	});
 });
 
-describe("real use md", () => {
-  it("should parse markdown table of contents", async () => {
-    const markdown = await readFile("tests/fixtures/test2.md", "utf-8");
-    const headersParsed = parseHeaders(markdown, "md");
+describe('real use md', () => {
+	it('should parse markdown table of contents', async () => {
+		const markdown = await readFile('tests/fixtures/test2.md', 'utf-8');
+		const headersParsed = parseHeaders(markdown, 'md');
 
-    expect(headersParsed).toStrictEqual([
-      { headerText: "Header 2", id: "#header-2", children: [] },
-      { headerText: "Header 2", id: "#header-2", children: [] },
-      {
-        headerText: "Header 2",
-        id: "#header-2",
-        children: [
-          { headerText: "Header 3", id: "#header-3", children: [] },
-          {
-            headerText: "Header 3",
-            id: "#header-3",
-            children: [
-              { headerText: "Header 4", id: "#header-4", children: [] },
-            ],
-          },
-          { headerText: "Header 3", id: "#header-3", children: [] },
-          { headerText: "Header 3", id: "#header-3", children: [] },
-          { headerText: "Header 3", id: "#header-3", children: [] },
-          { headerText: "Header 3", id: "#header-3", children: [] },
-          { headerText: "Header 3", id: "#header-3", children: [] },
-          { headerText: "Header 3", id: "#header-3", children: [] },
-        ],
-      },
-      { headerText: "Header 2", id: "#header-2", children: [] },
-    ]);
-  });
+		expect(headersParsed).toStrictEqual([
+			{ headerText: 'Header 2', id: 'header-2', children: [] },
+			{ headerText: 'Header 2', id: 'header-2', children: [] },
+			{
+				headerText: 'Header 2',
+				id: 'header-2',
+				children: [
+					{ headerText: 'Header 3', id: 'header-3', children: [] },
+					{
+						headerText: 'Header 3',
+						id: 'header-3',
+						children: [
+							{
+								headerText: 'Header 4',
+								id: 'header-4',
+								children: [],
+							},
+						],
+					},
+					{ headerText: 'Header 3', id: 'header-3', children: [] },
+					{ headerText: 'Header 3', id: 'header-3', children: [] },
+					{ headerText: 'Header 3', id: 'header-3', children: [] },
+					{ headerText: 'Header 3', id: 'header-3', children: [] },
+					{ headerText: 'Header 3', id: 'header-3', children: [] },
+					{ headerText: 'Header 3', id: 'header-3', children: [] },
+				],
+			},
+			{ headerText: 'Header 2', id: 'header-2', children: [] },
+		]);
+	});
 });
